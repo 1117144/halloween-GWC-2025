d8f8ee397f27c0031d5d73c8e9aca6f23754d36d
Yunhan Li <1117144@apps.nsd.org>
2025-10-23 23:22:38 +0000

    yeayy

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -4,3 +4,10 @@ st.title("Halloween Candy Recommender")
 
 df = pd.read_csv("candy-data.csv")
 sugar_level = st.slider("Sugar Level: " , 0, 10)
+df["sugar_scale"] = df["sugarpercent"] * 9 + 1
+candy_types = ["Chocolate", "Fruity", "Caramel", "Contain Nuts", "Nougat", "Crispy Wafer", "Hard", "Bar", "Puribus (one of many in a bag/box)"]
+selected_types = st.multiselect(
+    "What type/s of candy do you prefer?",
+    options = candy_types,
+    default = ["Chocolate"]
+)
